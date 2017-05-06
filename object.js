4a3f342cced428798af5e2770b10b0d1ede25536
Endhen <bogou.ncho@gmail.com>
2017-05-06 17:49:22 +0200

    Creation de l'objet 'Wave'

--- object.js
+++ object.js
@@ -1,12 +1,34 @@
+var x = 0;
+
 function Point(x, y) {
     this.x = x;
     this.y = y;
     this.pSize = 3;
-    
-    this.display = function() {
-        ellipse(this.y, this.x, this.pSize, this.pSize);
-    }
-    this.color = function() {
-        fill();
-    }
+
+    this.display = function () {
+        ellipse(this.x, this.y, this.pSize, this.pSize);
+    };
+}
+
+function Wave() {
+    this.points = []; //(x, y)
+    this.shape = 0; //y
+
+    this.create = function () {
+        for (x = 0; x < width; x++) {
+            this.shape = 50 * sin(x / 50) + 150;
+            this.points[x] = new Point(x, this.shape);
+        }
+    };
+
+    this.display = function () {
+        for (x = 0; x < this.points.length; x++) {
+            wave.points[x].display();
+        }
+
+        //refresh
+        if (this.points.length >= 600) {
+            this.points.splice(0, 600);
+        }
+    };
 }
--- sketch.js
+++ sketch.js
@@ -1,35 +1,22 @@
-var inc,
-    i,
-    osc = [],
-    wave,
+var i,
+    wave = new Wave(),
     mouseValue;
 
 function setup() {
     createCanvas(600, 300);
     frameRate(200);
-
 }
 
 function draw() {
     background(10);
     fill(255);
     noStroke();
+
+    //mouseValueY = map(mouseY, 0, 700, -100, 100);
+    //mouseValueX = map(mouseX, 0, 700, -100, 100);
     
-    mouseValueY = map(mouseY, 0, 700, -100, 100);
-    mouseValueX = map(mouseX, 0, 700, -100, 100);
     
-    for (i = 0; i < width; i++) {
-        wave = mouseValueY * sin(i / mouseValueX) + 150;
-        osc.push(new Point(wave, i));
-    }
+    wave.create();
+    wave.display();
     
-    //display
-    for (i = 0; i < osc.length; i++) {
-        osc[i].display();
-    }
-    
-    //refresh
-    if (osc.length >= 600) {
-        osc.splice(0, 600);
-    }
 }
